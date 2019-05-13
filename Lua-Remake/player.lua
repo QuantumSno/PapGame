d23a951aeaf6077987b407c6e871192b5c8dddea
QuantumSno <ben.czaja@protonmail.com>
2019-05-13 16:25:39 -0400

    pushing to home

--- Lua-Remake/player.lua
+++ Lua-Remake/player.lua
@@ -6,6 +6,8 @@ function loadPlayer()
     -- temp background
     background = love.graphics.newImage("assets/temp.jpg")
 
+    frame = 0
+
     character = {}
     character.x = width / 2
     character.y = height / 2
@@ -16,14 +18,28 @@ function loadPlayer()
     character.canMove = true
     character.canMoveUp = true
     character.direction = "down"
+    character.walking = false
     character.sprites = {}
     character.sprites.up = love.graphics.newImage("assets/Whyatt/whyatt up.png")
-    character.sprites.down = love.graphics.newImage(
-                                 "assets/Whyatt/whyatt down.png")
-    character.sprites.left = love.graphics.newImage(
-                                 "assets/Whyatt/whyatt left.png")
-    character.sprites.right = love.graphics.newImage(
-                                  "assets/Whyatt/whyatt right.png")
+    character.sprites.down = love.graphics.newImage("assets/Whyatt/whyatt down.png")
+    character.sprites.left = love.graphics.newImage("assets/Whyatt/whyatt left.png")
+    character.sprites.right = love.graphics.newImage("assets/Whyatt/whyatt right.png")
+
+
+    character.sprites.walking = {}
+    character.sprites.walking.up = {}
+    character.sprites.walking.up.input = love.graphics.newImage("assets/Whyatt/whyatt up walk.png")
+    character.sprites.walking.up.one = love.graphics.newQuad(0, 0, 32, 32, character.sprites.walking.up.input:getDimensions())
+    character.sprites.walking.up.two = love.graphics.newQuad(0, 0, 32, 32, 32, character.sprites.walking.up.input:getDimensions())
+    character.sprites.walking.down = {}
+    character.sprites.walking.down.input = love.graphics.newImage("assets/Whyatt/whyatt down walk.png")
+    character.sprites.walking.left = {}
+    character.sprites.walking.left.input = love.graphics.newImage("assets/Whyatt/whyatt left walk.png")
+    character.sprites.walking.right = {}
+    character.sprites.walking.rightinput = love.graphics.newImage("assets/Whyatt/whyatt right walk.png")
+
+
+
 
     function character:update()
         if love.keyboard.isDown("w") and character.canMove and
@@ -43,24 +59,47 @@ function loadPlayer()
             character.x = character.x + character.speed
             character.direction = "right"
         end
+
+        --update frames
+        frame = frame + 0.1
+
+
+
     end
 
     function character:draw()
         --temp background
         love.graphics.draw(background, 0, 0)
 
-        if character.direction == "up" then
-            love.graphics.draw(character.sprites.up, character.x, character.y,
-                               0, character.scale)
-        elseif character.direction == "down" then
-            love.graphics.draw(character.sprites.down, character.x, character.y,
-                               0, character.scale)
-        elseif character.direction == "left" then
-            love.graphics.draw(character.sprites.left, character.x, character.y,
-                               0, character.scale)
-        elseif character.direction == "right" then
-            love.graphics.draw(character.sprites.right, character.x,
-                               character.y, 0, character.scale)
+        love.graphics.draw(character.sprites.walking.up.input, character.sprites.walking.up.one, 0, 0, 0, character.scale)
+        if character.walking then
+            if character.direction == "up" then
+                love.graphics.draw(character.sprites.up, character.x, character.y,
+                                0, character.scale)
+            elseif character.direction == "down" then
+                love.graphics.draw(character.sprites.down, character.x, character.y,
+                                0, character.scale)
+            elseif character.direction == "left" then
+                love.graphics.draw(character.sprites.left, character.x, character.y,
+                                0, character.scale)
+            elseif character.direction == "right" then
+                love.graphics.draw(character.sprites.right, character.x,
+                                character.y, 0, character.scale)
+            end
+        else
+            if character.direction == "up" then
+                love.graphics.draw(character.sprites.up, character.x, character.y,
+                                0, character.scale)
+            elseif character.direction == "down" then
+                love.graphics.draw(character.sprites.down, character.x, character.y,
+                                0, character.scale)
+            elseif character.direction == "left" then
+                love.graphics.draw(character.sprites.left, character.x, character.y,
+                                0, character.scale)
+            elseif character.direction == "right" then
+                love.graphics.draw(character.sprites.right, character.x,
+                                character.y, 0, character.scale)
+            end
         end
 
     end
